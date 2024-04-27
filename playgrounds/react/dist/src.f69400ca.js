9e5d66ee28ee9202e9b7f332fb312b97f5297f39
Vaibhav Jain <vaibhavjain.fullstack@gmail.com>
2024-04-27 18:17:57 +0530

    setup dev script for all packages

--- playgrounds/react/dist/src.f69400ca.js
+++ playgrounds/react/dist/src.f69400ca.js
@@ -28440,7 +28440,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39771" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36195" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
